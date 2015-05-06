f808fefbcacaf6a04e97913f48aff88074d993a4
sergej.mueller <sergej.mueller@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-06 20:30:01 +0000

    Change of Author URI
    git-svn-id: https://plugins.svn.wordpress.org/snitch/trunk@1154840 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- snitch.php
+++ snitch.php
@@ -5,9 +5,9 @@ Description: Network monitor for WordPress. Connection overview for monitoring a
 Text Domain: snitch
 Domain Path: /lang
 Author: Sergej M&uuml;ller
-Author URI: http://wpcoder.de
+Author URI: https://sergejmueller.github.io
 Plugin URI: https://wordpress.org/extend/plugins/snitch/
-License: GPLv2 or later
+License: GPLv3 or later
 Version: 1.1.5
 */
 
@@ -16,7 +16,7 @@ Copyright (C)  2013-2015 Sergej Müller
 
 This program is free software; you can redistribute it and/or modify
 it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
+the Free Software Foundation; either version 3 of the License, or
 (at your option) any later version.
 
 This program is distributed in the hope that it will be useful,
